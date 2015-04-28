615032502cb7fcdd3d3e23e4b57221d3c7b766ed
Developer <fotoboxdevelopment2@gmail.com>
2015-04-28 12:58:54 +0800

    Testing instagram

--- resources/views/test/instagram.blade.php
+++ resources/views/test/instagram.blade.php
@@ -1,5 +1,5 @@
 @extends('app') 
 
 @section('content')
-	<img src="http://www.westerngazette.ca/wp-content/uploads/2015/03/222129-1r8o5m1400449246.png" width="100px" height="100px">
+	<a href="instagram://camera"><img src="http://www.westerngazette.ca/wp-content/uploads/2015/03/222129-1r8o5m1400449246.png" width="100px" height="100px"></a>
 @stop
